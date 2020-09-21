2da65ce2358acb146434017d27b62be44e5b60fd
partialblueshift <34142364+partialblueshift@users.noreply.github.com>
2020-09-21 22:09:39 +0200

    BN-84 Basic windows fixes for Addon/Manager (#85)
    * Make getTotalSpace and getAvailableSpace methods compatible with Windows
    * Do not register faulthandler in the Manager (not supported on Windows)
    
    Co-authored-by: Rabit <home@rabits.org>

--- manager.py
+++ manager.py
@@ -6,10 +6,6 @@ Description: REST interface for Manager
 Run: /srv/blender/blender -b -noaudio -P /srv/blendnet/manager.py
 '''
 
-import signal
-import faulthandler
-faulthandler.register(signal.SIGUSR1)
-
 import os, sys
 sys.path.append(os.path.dirname(__file__))
 
