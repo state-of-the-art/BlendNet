b66664f44a7882fa3092d68bdd87313f4d19fd23
Rabit <home@rabits.org>
2020-09-21 12:50:55 -0700

    BN-84 Removed the fault handler

--- manager.py
+++ manager.py
@@ -6,14 +6,6 @@ Description: REST interface for Manager
 Run: /srv/blender/blender -b -noaudio -P /srv/blendnet/manager.py
 '''
 
-import platform # Used to identify whether running on Linux or Windows
-
-import signal
-import faulthandler
-
-if platform.system() != 'Windows':
-    faulthandler.register(signal.SIGUSR1)
-
 import os, sys
 sys.path.append(os.path.dirname(__file__))
 
