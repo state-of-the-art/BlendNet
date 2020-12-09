d3ae4256034ffec39e8ca33c124c55149aa08782
Rabit <home@rabits.org>
2020-12-08 17:57:34 -0800

    Moved to v0.3.6 development process

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 bl_info = {
     'name': 'BlendNet - distributed cloud render',
     'author': 'www.state-of-the-art.io',
-    'version': (0, 3, 5),
+    'version': (0, 3, 6),
+    'warning': 'development version',
     'blender': (2, 80, 0),
     'location': 'Properties --> Render --> BlendNet Render',
     'description': 'Allows to easy allocate resources in cloud and '
