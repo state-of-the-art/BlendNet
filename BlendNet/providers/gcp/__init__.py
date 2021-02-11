ec253c67847869586b0ed69e0224fee889d03c86
Rabit <home@rabits.org>
2021-02-11 11:11:43 -0800

    BN-140 Removed gcp intel optimization due to issues on the other platforms

--- BlendNet/providers/gcp/__init__.py
+++ BlendNet/providers/gcp/__init__.py
@@ -396,7 +396,6 @@ systemctl start blendnet-manager.service # We don't need "enable" here
     data = {
         'name': cfg['instance_name'],
         'machineType': machine,
-        'minCpuPlatform': 'Intel Skylake', # Using the best option
         'description': 'BlendNet Agents Manager',
         'labels': {
             'app': 'blendnet',
@@ -519,7 +518,6 @@ systemctl start blendnet-agent.service # We don't need "enable" here
     data = {
         'name': cfg['instance_name'],
         'machineType': machine,
-        'minCpuPlatform': 'Intel Skylake', # Using the best option
         'description': 'BlendNet Agent worker',
         'scheduling': {
             'preemptible': cfg['use_cheap_instance'],
